2151465250922db6112d67c330904c839ced6920
IsBebs <banangurkkungen@gmail.com>
2024-03-09 11:45:57 +0100

    Added Sprey bullet and damage interface, health, etc

---
+++ Assets/Scripts/IDamage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamage 
+{
+    public void Damage(int damage);
+    // Start is called before the first frame update
+
+}
