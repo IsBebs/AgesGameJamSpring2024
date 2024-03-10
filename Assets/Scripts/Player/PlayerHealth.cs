488416e98f1a349574991ec675346510cca98096
IsBebs <banangurkkungen@gmail.com>
2024-03-10 11:07:51 +0100

    Added player sound

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,10 +8,25 @@ public class PlayerHealth : MonoBehaviour, IDamage
     float LastDamageTime = 0;
     [SerializeField]
     int health;
+    float DamageInterValTimer =0;
     [SerializeField]
     float DamageInterVal;
     [SerializeField]
     TextMeshProUGUI HealthText;
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    AudioClip[] audioClips;
+    [SerializeField]
+    float DamageSpriteAlpha1;
+    [SerializeField]
+    float DamageSpriteAlpha2;
+    bool useDamageSpriteAlpha1 = true;
+    [SerializeField]
+    float DamageSpriteAlphaFlipTime;
+    float DamageSpriteAlphaFlipTimer = 0;
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
 
     private void Start()
     {
@@ -20,13 +35,19 @@ public class PlayerHealth : MonoBehaviour, IDamage
 
     public void Damage(int damage)
     {
-        if (DamageInterVal < Time.time - LastDamageTime)
+        if (DamageInterValTimer <= 0)
         {
+            DamageInterValTimer = DamageInterVal;
             health -= damage;
             HealthText.text = $"Health:{health}";
             if (health <= 0)
             {
                 gameObject.SetActive(false);
+            }
+            else
+            {
+                int randomIndex = Random.Range(0, audioClips.Length);
+                audioSource.PlayOneShot(audioClips[randomIndex]);
             }
             Debug.LogWarning("Damage");
             LastDamageTime = Time.time;
@@ -42,5 +63,30 @@ public class PlayerHealth : MonoBehaviour, IDamage
     public bool SetCombo(int combo)
     {
         throw new System.NotImplementedException();
+    }
+    private void Update()
+    {
+        if (DamageInterValTimer > 0)
+        {
+            DamageInterValTimer -= Time.deltaTime;
+            DamageSpriteAlphaFlipTimer -= Time.deltaTime;
+            if (DamageSpriteAlphaFlipTimer <= 0)
+            {
+                DamageSpriteAlphaFlipTimer = DamageSpriteAlphaFlipTime;
+                Color tempColor = spriteRenderer.color;
+                tempColor.r = 5;
+                useDamageSpriteAlpha1 = !useDamageSpriteAlpha1;
+                tempColor.a = useDamageSpriteAlpha1 ? DamageSpriteAlpha1 : DamageSpriteAlpha2;
+                spriteRenderer.color = tempColor;
+            }
+        }
+        else if (DamageInterValTimer <= 0 && spriteRenderer.color.a != 1)
+        {
+            Color tempColor = spriteRenderer.color;
+            tempColor.a = 1;
+            tempColor.r = 1;
+            spriteRenderer.color = tempColor;
+
+        }
     }
 }
